458e25cc9c32e9994ac6b48809baa7accc88f2e6
bigthyme <asinghavong@gmail.com>
2013-01-22 08:05:56 -0800

    Creating production GET calls with route /NEW

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -15,33 +15,33 @@ class LeaderboardController < ApplicationController
     
     def new
 
-    #Team GET Call and store
-        # team_response = HTTParty.get('http://br-interview-api.heroku.com/teams')
-        # team_data = JSON.parse(team_response)
+    # Team GET Call and store
+        team_response = HTTParty.get('http://br-interview-api.heroku.com/teams')
+        team_data = JSON.parse(team_response)
         
-        # team_data.each do |line|
-        #   t = Team.new
-        #   t.name = line['team']['name']
-        #   t.nick = line['team']['nick']
-        #   t.api_team_id = line['team']['id']
+        team_data.each do |line|
+          t = Team.new
+          t.name = line['team']['name']
+          t.nick = line['team']['nick']
+          t.api_team_id = line['team']['id']
           
-        #   t.save
-        # end
+          t.save
+        end
 
-    #User GET Call and store
-        # user_response = HTTParty.get('http://br-interview-api.heroku.com/users')
-        # user_data = JSON.parse(user_response)
+    # User GET Call and store
+        user_response = HTTParty.get('http://br-interview-api.heroku.com/users')
+        user_data = JSON.parse(user_response)
         
-        # user_data.each do |line|
-        #   u = User.new
-        #   u.first_name = line['user']['first_name']
-        #   u.last_name = line['user']['last_name']
-        #   u.api_user_id = line['user']['id']  
-        #   u.points = line['user']['points']
-        #   u.points_last_week = line['user']['points_last_week']
+        user_data.each do |line|
+          u = User.new
+          u.first_name = line['user']['first_name']
+          u.last_name = line['user']['last_name']
+          u.api_user_id = line['user']['id']  
+          u.points = line['user']['points']
+          u.points_last_week = line['user']['points_last_week']
           
-        #   u.save
-        # end 
+          u.save
+        end 
 
     #Favorite GET Call and store
         fav_response = HTTParty.get('http://br-interview-api.heroku.com/favorites')
